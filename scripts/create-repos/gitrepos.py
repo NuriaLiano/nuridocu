c8d45562daffc9ee46e5be5037a186606afa2801
Nuria Liaño <lianonuria@gmail.com>
2023-03-30 21:30:38 +0200

    add more features to gitrepos

--- scripts/create-repos/gitrepos.py
+++ scripts/create-repos/gitrepos.py
@@ -7,15 +7,25 @@
 #git commit
 #git push
 
-
-
 import os
 import sys
+import requests
 
 #global const
 DNAME = ""
 DMODE = 776
-GITPATH = "/home/glianon/gitlab/"
+#GITPATH = "/home/glianon/gitlab/"
+GITPATH = "C:/Users/nuria-msi/gitlab/"
+
+# GitLab API URL and token
+GITLAB_URL = "https://gitlab.com/api/v4"
+GITLAB_TOKEN = "glpat-zggGt6HzmgAjvm4JNEg9"
+# GitLab project namespace
+NAMESPACE = "7736983"
+# GitLab repository default branch name
+DEFAULT_BRANCH = "main"
+# GitLab repository visibility
+VISIBILITY = "public"
 
 #change spaces for lines
 def replaceSpaces(reponame):
@@ -29,7 +39,6 @@ def replaceSpaces(reponame):
 #check if that directory exists
 def existsDirectory():
     try:
-        print(os.path.exists(GITPATH + DNAME))
         return os.path.exists(GITPATH + DNAME)
     except FileExistsError():
         print("ERROR | El directorio ya existe") 
@@ -40,24 +49,129 @@ def createDirectory():
     except FileExistsError():
         print("ERROR | El directorio ya existe") 
     
-#verify that a folder name has been passed as a parameter
-if len(sys.argv) < 2:
-    #order the folder name
-    print("Dime el nombre del repositorio: ")
-    DNAME = replaceSpaces(input())
-    while existsDirectory():
+def createLocalRepo():
+
+    !IMPORTANTE PRIMERO HAY QUE HACER UN CLONE DEL REPO DE GITLAB EN LOCAL, NO VALE CREAR LA CARPETA PRIMERO¡
+
+    #verify that a folder name has been passed as a parameter
+    if len(sys.argv) < 2:
+        #order the folder name
+        print("Dime el nombre del repositorio: ")
         DNAME = replaceSpaces(input())
-    createDirectory()
-    print(os.listdir(GITPATH))
-    print("CORRECTO. El directorio se ha creado")
-else:
-    #extract repo name for the arguments
-    DNAME = replaceSpaces(sys.argv[1])
-    if (existsDirectory()):
+        while existsDirectory():
+            print("Prueba con otro nombre: ")
+            DNAME = replaceSpaces(input())
         createDirectory()
-        print("CORRECTO | El directorio se ha creado")
+        #print(os.listdir(GITPATH))
+        print("CORRECTO. El directorio se ha creado")
+    else:
+        #extract repo name for the arguments
+        DNAME = replaceSpaces(sys.argv[1])
+        while existsDirectory():
+            print("Prueba con otro nombre: ")
+            DNAME = replaceSpaces(input())
+        createDirectory()
+        #print(os.listdir(GITPATH))
+        print("CORRECTO. El directorio se ha creado")
+
+# def get_user_id(username):
+#     try:
+#         # Set the API endpoint  
+#         url = GITLAB_URL + "users?username="+username
+        
+#         #set the request headers and data
+#         headers = {"PRIVATE-TOKEN": GITLAB_TOKEN}
+        
+#         # Send the GET request to get the user ID
+#         response = requests.get(url, headers=headers)
+
+#         # Parse the response JSON and return the user ID
+#         return response.json()[0]["id"]
+
+#     except requests.exceptions.RequestException as e:
+#         print("ERROR | Ha ocurrido un error al obtener el ID de usuario: ", e)
+#         return None
+
+# def namespace_exists(username):
+#     try:
+#         # Get the user ID
+#         user_id = get_user_id(username)
+
+#         # Set the API endpoint
+#         url = GITLAB_URL + "users/" + str(user_id) + "/projects"
+
+#         # Set the request headers
+#         headers = {"PRIVATE-TOKEN": GITLAB_TOKEN}
+
+#         # Send the GET request to get the projects for the user
+#         response = requests.get(url, headers=headers)
+
+#         # Parse the response JSON and check if any project has the namespace equal to the username
+#         projects = response.json()
+#         for project in projects:
+#             if project["namespace"]["name"] == username:
+#                 return True
+
+#         return False
+#     except requests.exceptions.RequestException as e:
+#         print("ERROR | Ha ocurrido un error al obtener los proyectos del usuario: ", e)
+#         return False
+
+def createGitLabRepo():
+    try:
+        # Set the API endpoint  
+        url = GITLAB_URL + "/projects"
+
+        #set the request headers and data
+        headers = {"PRIVATE-TOKEN": GITLAB_TOKEN}
+        data = {
+            "name": DNAME,
+            "namespace": {
+                "id": NAMESPACE
+            },
+            "default_branch": DEFAULT_BRANCH,
+            "visibility": VISIBILITY
+        }
+
+        #send the POST request to create the repository
+        response = requests.post(url, headers=headers, data=data)
+
+        #debug to fix errors with api
+        #print(response.status_code)
+        #print(response.json())
+
+        #check if the request was successful
+        if response.status_code == 201:
+            return True
+        else:
+            print("ERROR | No se ha podido crear el repositorio en GitLab")
+            return False
+    except requests.exceptions.RequestException as e:
+        print("ERROR | Ha ocurrido un error al crear el repositorio: ", e)
+        return False
+
+def gitInitLocalRepo():
 
 
+def createInitialReadme():
+
+    # Crear archivo README.md en la carpeta local del repositorio
+    with open(os.path.join(GITPATH, DNAME, "README.md"), "w") as f:
+        f.write("# " + DNAME)
+
+    # Agregar archivo README.md al repositorio local
+    os.system(f"cd {os.path.join(GITPATH, DNAME)} && git add README.md")
+
+    # Hacer un commit y push al repositorio remoto
+    os.system(f"cd {os.path.join(GITPATH, DNAME)} && git commit -m 'Agregando archivo README.md' && git push")
+
+
+def main():
+    createLocalRepo()
+    createGitLabRepo()
+    createInitialReadme()
+
+main()
 
 
     
