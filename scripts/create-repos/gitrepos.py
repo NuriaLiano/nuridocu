a5242061853477f2dfc208bcbccb749ee968b43e
Nuria Liaño <lianonuria@gmail.com>
2023-05-06 21:33:34 +0200

    ADD CHANGES

--- scripts/create-repos/gitrepos.py
+++ scripts/create-repos/gitrepos.py
@@ -1,102 +1,129 @@
 import configparser, sys, gitlab, os, git, shutil
 
-#TODO: revisar que pasa con get_token
+#TODO: NO FUNCIONA ELIMINAR
 #TODO: añadir que compruebe el git config y si no le configure
-#TODO: revisar para meter las variables importantes en .config
-#TODO: preguntar si va a eliminar o crear repo
-#TODO: crear fichero con las dependencias que haya que instalar
-#pip install gitpython
-#pip install python-gitlab
 #TODO: al borrar el repo preguntar para borrar la carpeta local
 
+def load_config():
 
-# def get_token():
-#     #read config file
-#     config = configparser.ConfigParser()
-#     config.read('.config')
-#     #obtain token
-#     print(config.sections())
-#     return config.get('gitlab', 'token')
+    #global vars
+    global GL_TK, URL_GL, INSTANCE_GL
+
+    #read config file
+    config = configparser.ConfigParser()
+    config.read('.config')
+    #obtain token
+    GL_TK = config.get('DEFAULT', 'GL_TOKEN')
+    URL_GL = config.get('DEFAULT', 'URL_GL')
+    print(URL_GL, GL_TK)
+    INSTANCE_GL = gitlab.Gitlab('https://gitlab.com', private_token=GL_TK)
+
 
 def get_repo_name():
+    #global vars
+    global REPO_NAME
+
     #read repo_name from parameter
     if len (sys.argv) > 1:
-        repo_name = sys.argv[1]
+        REPO_NAME = sys.argv[1].replace(' ', '-')
     else:
         while True:
-            repo_name = input('Enter the repos name: ')
-            if repo_name:
-                return repo_name
+            repo_name_input = input(Fore.CYAN + 'Enter the repos name: ')
+            if repo_name_input != '':
+                REPO_NAME = repo_name_input.replace(' ', '-')
+                break
 
-def create_repo(repo_name, token):
+def get_local_dir():
+    #global vars
+    global LOCAL_DIR, LOCAL_PATH_REPO
+    while True:
+        local_dir_input = input(Fore.CYAN + 'Enter the local path: ')
+        if local_dir_input != '':
+            LOCAL_DIR = local_dir_input.replace(' ', '-')
+            LOCAL_PATH_REPO = os.path.join(LOCAL_DIR, REPO_NAME)
+            break
+
+def create_repo():
+    global PROJECT_URL
     #create object with token
-    gl = gitlab.Gitlab('https://gitlab.com', private_token=token)
-    #repo_name = 'eliminar'
+    gl = INSTANCE_GL
     #create new repo
-    project = gl.projects.create({'name': repo_name})
+    project = gl.projects.create({'name': REPO_NAME})
     #print url
-    print('Check the new repo: ', project.web_url)
+    PROJECT_URL = project.http_url_to_repo
+    print(Fore.YELLOW + '[CHECK]' + 'Check the new repo: ', project.web_url)
     return project
 
-def search_repo(repo_name, token):
+def search_repo():
     #search project by name
-    gl = gitlab.Gitlab('https://gitlab.com', private_token=token)
-    projects = gl.projects.list(search=repo_name)
-    if not projects:
-        print(f'No projects found with name "{repo_name}"')
-        return
-    return projects[0]
+    gl = INSTANCE_GL
+    try:
+        projects = gl.projects.list(search=REPO_NAME)
+    except gitlab.exceptions.GitlabError as e:
+        print("GitLab API returned an error:", e)
+    return
+    # if not projects:
+    #     print(Fore.RED + '[ERROR]' + f'No projects found with name "{REPO_NAME}"')
+    #     return
+    # return projects[0]
 
-def remove_repo(repo_name, token):
+def remove_repo():
     remove_prompt = input ('Are you sure you want to delete the repository? (y/n)')
     if remove_prompt.lower() == 'y':
-        project = search_repo(repo_name, token)
+        project = search_repo()
         if project is None:
-            print(f'No project found with name "{repo_name}"')
+            print(Fore.RED + '[ERROR]' + f'No project found with name "{REPO_NAME}"')
             return
-        gl = gitlab.Gitlab('https://gitlab.com', private_token=token)
+        gl = INSTANCE_GL
         gl.projects.delete(project.id)
-        print('The repo ' + repo_name + ' has been deleted')
+        print(Fore.GREEN + '[DONE]' + 'The repo ' + REPO_NAME + ' has been deleted')
     else:
-        print('The repo ' + repo_name + ' has not been deleted')
+        print(Fore.RED + '[ERROR]' + 'The repo ' + REPO_NAME + ' has not been deleted')
 
-def remove_local_directory(dir_name):
-    if os.path.exists(dir_name):
-        shutil.rmtree(dir_name)
-        print(f'Directory "{dir_name}" has been deleted')
+def remove_local_directory():
+    if os.path.exists(LOCAL_PATH_REPO):
+        shutil.rmtree(LOCAL_PATH_REPO)
+        print(f'Directory "{LOCAL_PATH_REPO}" has been deleted')
     else:
-        print(f'Directory "{dir_name}" does not exist')
+        print(Fore.RED + '[ERROR]' + f'Directory "{LOCAL_PATH_REPO}" does not exist')
 
-def create_local_directory(dir_name):
-    os.mkdir(dir_name)
-    print(f'Local folder "{dir_name}" created successfully')
+def create_local_directory():
+    os.mkdir(LOCAL_PATH_REPO)
+    print(f'Local folder "{LOCAL_PATH_REPO}" created successfully')
 
-def clone_repo(repo_url, dir_path):
-    git.Repo.clone_from(repo_url, dir_path)
-    print(f'Repository cloned to local folder "{dir_path}" successfully')
+def clone_repo():
+    git.Repo.clone_from(PROJECT_URL, LOCAL_PATH_REPO)
+    print(f'Repository cloned to local folder "{LOCAL_PATH_REPO}" successfully')
 
 if __name__ == '__main__':
-    
-    token = ""
+
+    #colorama
+    from colorama import init, Fore, Back, Style
+    init(autoreset=True)
 
     main_prompt = input('Do you want remove or create new repo (r/c)')
     
-    # token = get_token()
-    repo_name = get_repo_name()
+    #load all vars and config
+    load_config()
+
+    get_repo_name()
+    get_local_dir()
+
+    print(LOCAL_PATH_REPO)
 
     if main_prompt == 'r':
         print('YOU HAVE CHOSEN TO REMOVE THE REPO AND DIRECTORY. ¡¡¡¡THIS ACTION CANNOT BE UNDONE!!!!')
         confirm_prompt = input('Are you sure? (y/n)')
         if confirm_prompt == 'y':
-            remove_repo(repo_name, token)
-            remove_local_directory(repo_name)
+            remove_repo()
+            remove_local_directory()
         else:
             print('Oh my god you almost messed up')
     else:
         print('YOU HAVE CHOSEN TO CREATE NEW REPO.')
-        project = create_repo(repo_name, token)
-        create_local_directory(repo_name)
-        clone_repo(project.http_url_to_repo, repo_name)
+        project = create_repo()
+        create_local_directory()
+        clone_repo()
     
 
     
